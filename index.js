2565d6f66a64a6a9a70d80c5d838c7d47cc44a55
PepeDeSoto <pepesoto240@gmail.com>
2021-03-18 10:18:19 +0100

    inicializar carpeta public index.html

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-var cool = require ("cool-ascii-faces");
+// var cool = require ("cool-ascii-faces");
 var express = require ("express");
+var path = require("path");
 
 var app = express();
-
 var port = 10000;
 
-app.get("/cool",(request,response) => {
-	response.send(cool());
-	console.log("New request to /cool hasta arrived");
-});
+// app.get("/cool",(request,response) => {
+// 	response.send(cool());
+// 	console.log("New request to /cool hasta arrived");
+// });
+
+app.use("/",express.static(path.join(__dirname,"public")));
+
+// app.get("/",(req,res) =>{
+// 	res.render("/index"); //corregir la renderizacion
+// });
 
 app.listen(port,() => {
-	console.log("Server ready lintening on port "+port);
+	console.log(`Server ready lintening on port ${port}`);
 })
+
