b53e552dbd12b04365d520a23ed39e9d5dfc96b5
AlbertoTNJ <jtrancoso@gmail.com>
2021-05-05 23:11:19 +0200

    updated pagination

--- src/SalesAPI.svelte
+++ src/SalesAPI.svelte
@@ -1,5 +1,4 @@
 <script>
-    
     import {
         Nav,
         Modal,
@@ -18,8 +17,11 @@
         CardSubtitle,
         CardText,
         CardTitle,
+        Pagination,
+    PaginationItem,
+    PaginationLink
     } from "sveltestrap";
-
+    import { onMount } from "svelte";
     //Load
     let open1 = false;
     const toggle1 = () => (open1 = !open1);
@@ -48,6 +50,14 @@
         secondhand: "",
     };
     let error = null;
+    let alertError = "";
+    let alertOk = "";
+    //Pagination
+    let current_offset = 0;
+    let limit = 10;
+    let current_page = 1;
+    let last_page = 1;
+    let total = 0;
 
     async function insertData() {
         console.log("Inserting data: " + JSON.stringify(newSales));
@@ -95,12 +105,13 @@
                 error = 404;
                 console.log("Error");
             }
+            getNumData();
         });
     }
 
     async function getData() {
         console.log("Fetching data...");
-        const res = await fetch("api/v1/sales/");
+        const res = await fetch("api/v1/sales?limit="+limit+"&offset="+current_offset);
         if (res.ok) {
             console.log("Ok");
             const json = await res.json();
@@ -114,7 +125,40 @@
         } else {
             console.log("Error");
         }
+        getNumData();
     }
+
+
+    async function getNumData() {
+    const res = await fetch("api/v1/sales");
+    if (res.ok) {
+      const json = await res.json();
+      total = json.length;
+      console.log("Number of stats : " + total);
+      changePage(current_page, current_offset);
+    } else {
+      alertError = "No se han encontrado datos.";
+    }
+  }
+   //Calcula el rango entre dos valores
+  function range(size, startAt = 0) {
+    return [...Array(size).keys()].map((i) => i + startAt);
+  }
+  function changePage(page, offset) {
+    console.log("------Change page------");
+    console.log("Params page: " + page + " offset: " + offset);
+    last_page = Math.ceil(total / 10);
+    console.log("new last page: " + last_page);
+    if (page !== current_page) {
+      current_offset = offset;
+      current_page = page;
+      console.log("page: " + page);
+      console.log("current_offset: " + current_offset);
+      console.log("current_page: " + current_page);
+      getData();
+    }
+    console.log("---------Exit change page-------");
+  }
     async function deleteData() {
         console.log("Deleting data...");
         const res = await fetch("api/v1/sales/", {
@@ -123,6 +167,8 @@
             if (res.ok) {
                 console.log("OK");
                 salesData = [];
+                console.log("llego aqui");
+                getNumData();
                 error = 0;
             } else if ((res.status = 404)) {
                 error = 404;
@@ -132,14 +178,13 @@
                 console.log("Error");
             }
         });
+        
     }
     async function deleteResource(location, year) {
         console.log(`Deleting data`);
-        const res = await fetch("/api/v1/sales/" + location + "/" + year,
-            {
-                method: "DELETE",
-            }
-        ).then(function (res) {
+        const res = await fetch("/api/v1/sales/" + location + "/" + year, {
+            method: "DELETE",
+        }).then(function (res) {
             if (res.ok) {
                 console.log("OK");
                 if (salesData.length == 1) {
@@ -148,15 +193,18 @@
                 }
                 console.log(`Resource has been deleted`);
                 getData();
+                getNumData();
             } else {
                 if (res.status == 404) {
-                    console.log=(`Resource not found`);
+                    console.log = `Resource not found`;
                 } else if (res.status == 500) {
                     console.log(`Database error`);
                 }
             }
         });
     }
+    onMount(getData);
+    getNumData();
 </script>
 
 <main>
@@ -166,21 +214,30 @@
     <div class="container">
         <Nav>
             <NavItem>
-                <NavLink href="/">Volver</NavLink>
+                <a href="/"><Button color="primary">Volver</Button></a>
             </NavItem>
             <NavItem>
-                <NavLink href="#" on:click={toggle1}
-                    >Cargar datos inciales</NavLink>
+                <Button color="success" on:click={toggle1}
+                    >Cargar datos inciales</Button
+                >
                 <Modal isOpen={open1} {toggle1}>
                     <ModalHeader {toggle1}>
-                        <p style="color:black"><b>¿Cargar los datos iniciales?</b></p></ModalHeader>
+                        <p style="color:black">
+                            <b>¿Cargar los datos iniciales?</b>
+                        </p></ModalHeader
+                    >
                     <ModalBody>
-                        <p style="color:black"><b>Esta acción cargará los datos siempre y cuando no
-                            existan previamente.</b></p>
+                        <p style="color:black">
+                            <b
+                                >Esta acción cargará los datos siempre y cuando
+                                no existan previamente.</b
+                            >
+                        </p>
                     </ModalBody>
                     <ModalFooter>
                         <Button color="primary" on:click={toggle1P}
-                            >Cargar</Button>
+                            >Cargar</Button
+                        >
                         <Button color="secondary" on:click={toggle1}>
                             Cancelar
                         </Button>
@@ -189,13 +246,17 @@
             </NavItem>
             <NavItem>
                 {#if salesData.length == 0}
-                    <NavLink disabled href="#" on:click={toggle2}
+                    <Button color="danger" on:click={toggle2}
+                    >Borrar todos los datos</Button>
+                  <!--  <NavLink disabled href="#" on:click={toggle2}
                         >Borrar todos los datos</NavLink
-                    >
+                    >-->
                 {:else}
-                    <NavLink href="#" on:click={toggle2}
+                <Button color="danger" on:click={toggle2}
+                >Borrar todos los datos</Button>
+                    <!--<NavLink href="#" on:click={toggle2}
                         >Borrar todos los datos</NavLink
-                    >
+                    >-->
                     <Modal isOpen={open2} {toggle2}>
                         <ModalHeader {toggle2}
                             >¿Borrar todos los datos?</ModalHeader
@@ -219,6 +280,7 @@
             <UncontrolledAlert color="success">
                 Operación realizada correctamente.
             </UncontrolledAlert>
+            
         {/if}
 
         {#if error == 409}
@@ -234,56 +296,134 @@
                 >Error desconocido.</UncontrolledAlert
             >
         {/if}
-    <p>
-        No se han encontrado datos, por favor carga los datos iniciales.
-    </p>
         <!-- Table -->
-    <table responsive="true" bordered="true">
-        <thead>
-            <tr>
-                <th>Localizacion</th>
-                <th>Año</th>
-                <th>Total</th>
-                <th>Viviendas protegidas</th>
-                <th>Nuevas</th>
-                <th>Segunda mano</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr class="insertadatos">
-                <td><input type="text" placeholder="Localizacion" bind:value={newSales.location}/></td>
-                <td><input type="text" placeholder="Año" bind:value={newSales.year}/></td>
-                <td><input type="text" placeholder="Total" min="0" bind:value={newSales["total"]}/></td>
-                <td><input type="text" placeholder="Viviendas protegidas" min="0" bind:value={newSales["protectedhousing"]}/></td>
-                <td><input type="text" placeholder="Nuevas" min="0" bind:value={newSales["new"]}/></td>
-                <td><input type="text" placeholder="Segunda mano" min="0" bind:value={newSales["secondhand"]}/></td>
-                <td><Button color="primary" on:click={insertData}>Insertar</Button></td>
-            </tr>
-            {#if salesData.length != 0}
-                {#each salesData as data}
-                    <tr>
-                        <td>{data.location}</td>
-                        <td>{data.year}</td>
-                        <td>{data["total"]}</td>
-                        <td>{data["protectedhousing"]}</td>
-                        <td>{data["new"]}</td>
-                        <td>{data["secondhand"]}</td>
-                        <!--<td><span class="editar" onclick="transformarEnEditable(this)">Editar</span></td>-->
-                        <td><a href="#/sales/{data.location}/{data.year}"><Button color="primary">Editar</Button></a></td>
-                        <td><Button color="danger" on:click={deleteResource(data.location, data.year)}>Borrar</Button></td>
-                    </tr>
-                {/each}
-            {/if}
-        </tbody>
-    </table>
-    <div id="contenedorForm"></div>
-       
+        <table responsive="true" bordered="true">
+            <thead>
+                <tr>
+                    <th>Localizacion</th>
+                    <th>Año</th>
+                    <th>Total</th>
+                    <th>Viviendas protegidas</th>
+                    <th>Nuevas</th>
+                    <th>Segunda mano</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="insertadatos">
+                    <td
+                        ><input
+                            type="text"
+                            placeholder="Localizacion"
+                            bind:value={newSales.location}
+                        /></td
+                    >
+                    <td
+                        ><input
+                            type="text"
+                            placeholder="Año"
+                            bind:value={newSales.year}
+                        /></td
+                    >
+                    <td
+                        ><input
+                            type="text"
+                            placeholder="Total"
+                            min="0"
+                            bind:value={newSales["total"]}
+                        /></td
+                    >
+                    <td
+                        ><input
+                            type="text"
+                            placeholder="Viviendas protegidas"
+                            min="0"
+                            bind:value={newSales["protectedhousing"]}
+                        /></td
+                    >
+                    <td
+                        ><input
+                            type="text"
+                            placeholder="Nuevas"
+                            min="0"
+                            bind:value={newSales["new"]}
+                        /></td
+                    >
+                    <td
+                        ><input
+                            type="text"
+                            placeholder="Segunda mano"
+                            min="0"
+                            bind:value={newSales["secondhand"]}
+                        /></td
+                    >
+                    <td
+                        ><Button color="primary" on:click={insertData}
+                            >Insertar</Button
+                        ></td
+                    >
+                </tr>
+                {#if salesData.length != 0}
+                    {#each salesData as data}
+                        <tr>
+                            <td>{data.location}</td>
+                            <td>{data.year}</td>
+                            <td>{data["total"]}</td>
+                            <td>{data["protectedhousing"]}</td>
+                            <td>{data["new"]}</td>
+                            <td>{data["secondhand"]}</td>
+                            <!--<td><span class="editar" onclick="transformarEnEditable(this)">Editar</span></td>-->
+                            <td
+                                ><a href="#/sales/{data.location}/{data.year}"
+                                    ><Button color="primary">Editar</Button></a
+                                ></td
+                            >
+                            <td
+                                ><Button
+                                    color="danger"
+                                    on:click={deleteResource(
+                                        data.location,
+                                        data.year
+                                    )}>Borrar</Button
+                                ></td
+                            >
+                        </tr>
+                    {/each}
+                {/if}
+            </tbody>
+        </table>
+        <div id="contenedorForm" />
     </div>
+    <Pagination ariaLabel="Web pagination">
+        <PaginationItem class={current_page == 1 ? "disabled" : ""}>
+          <PaginationLink
+            previous
+            href="#/sales"
+            on:click={() => changePage(current_page - 1, current_offset - 10)}
+          />
+        </PaginationItem>
+        {#each range(last_page, 1) as page}
+          <PaginationItem class={current_page == page ? "active" : ""}>
+            <PaginationLink
+              previous
+              href="#/sales"
+              on:click={() => changePage(page, (page - 1) * 10)}
+              >{page}</PaginationLink
+            >
+          </PaginationItem>
+        {/each}
+        <PaginationItem class={current_page == last_page ? "disabled" : ""}>
+          <PaginationLink
+            next
+            href="#/sales"           
+            on:click={() => changePage(current_page + 1, current_offset + 10)}
+          />
+        </PaginationItem>
+      </Pagination>
 </main>
 
 <style>
-    *{
-        margin:0;
+    * {
+        margin: 0;
         padding: 0;
     }
     main {
@@ -333,20 +473,20 @@
         line-height: 30px;
         margin: 15px;
     }
-    .repourl {
+    /*.repourl {
         display: grid;
         grid-template-columns: 1fr 1fr;
-    }
-    .group {
+    }*/
+    /*.group {
         display: grid;
         grid-template-columns: 1fr 1fr 1fr;
-    }
+    }*/
 
     @media (max-width: 1080px) {
-        .group {
+        /*.group {
             display: grid;
             grid-template-columns: 1fr;
-        }
+        }*/
     }
 
     table {
