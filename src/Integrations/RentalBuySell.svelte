68018e7d2c3e6c874b04b5e195e43e56a9eb0ae1
PepeDeSoto <pepesoto240@gmail.com>
2021-06-03 19:52:12 +0200

    realizando integracion compraventas

--- src/Integrations/RentalBuySell.svelte
+++ src/Integrations/RentalBuySell.svelte
@@ -1,45 +1,38 @@
 <script>
+    import bb from "billboard.js/dist/billboard.pkgd";
     import { onMount } from "svelte";
-    import {
-        Nav,
-        NavItem,
-        NavLink,
-        Button,
-    } from "sveltestrap";
+    import { Nav, NavItem, NavLink, Button } from "sveltestrap";
+    import App from "../App.svelte";
 
-    let UnemploymentData = [];
-    let UnemploymentLocation = []
-    let UnemploymentYear = [];
-    let unemploymentTotal = 0;
-    let salesData = [];
-    let salesLocation = [];
-    let salesYear = [2015, 2016, 2017, 2018, 2019];
+    let year = [];
     let salesTotalData2015 = 0;
     let salesTotalData2016 = 0;
     let salesTotalData2017 = 0;
     let salesTotalData2018 = 0;
     let salesTotalData2019 = 0;
-    
-
 
     async function getDataGraph() {
         console.log("Fetching data...");
-        const res = await fetch("https://sos2021-07.herokuapp.com/api/v2/buy_sell");
+        const res = await fetch(
+            "https://sos2021-07.herokuapp.com/api/v2/buy_sell"
+        );
         if (res.ok) {
             console.log("Ok");
             const json = await res.json();
             UnemploymentData = json;
-            UnemploymentData.sort((a, b) => (a.year > b.year) ? 1 : -1)
-            UnemploymentData.sort((a, b) => (a.location > b.location) ? 1 : -1)
-            console.log(`We have received ${UnemploymentData.length} resources.`);
+            UnemploymentData.sort((a, b) => (a.year > b.year ? 1 : -1));
+            UnemploymentData.sort((a, b) => (a.location > b.location ? 1 : -1));
+            console.log(
+                `We have received ${UnemploymentData.length} resources.`
+            );
             UnemploymentData.forEach((data) => {
-                if (data.year == 2020){
-                    unemploymentTotal=unemploymentTotal+parseFloat(data["unemployment_rate"]);
+                if (data.year == 2020) {
+                    unemploymentTotal =
+                        unemploymentTotal +
+                        parseFloat(data["unemployment_rate"]);
                 }
             });
-            console.log("Rate: "+unemploymentTotal);
-
-            
+            console.log("Rate: " + unemploymentTotal);
         } else {
             console.log("Error");
         }
@@ -48,26 +41,30 @@
             console.log("Ok");
             const json2 = await res2.json();
             salesData = json2;
-            salesData.sort((a, b) => (a.year > b.year) ? 1 : -1)
-            salesData.sort((a, b) => (a.location > b.location) ? 1 : -1)
+            salesData.sort((a, b) => (a.year > b.year ? 1 : -1));
+            salesData.sort((a, b) => (a.location > b.location ? 1 : -1));
             console.log(`We have received ${salesData.length} resources.`);
-            
+
             salesData.forEach((data) => {
-                
-                if (data.year == 2015){
-                    salesTotalData2015=salesTotalData2015+parseInt(data["total"]);
+                if (data.year == 2015) {
+                    salesTotalData2015 =
+                        salesTotalData2015 + parseInt(data["total"]);
                 }
-                if (data.year == 2016){
-                    salesTotalData2016=salesTotalData2016+parseInt(data["total"]);
+                if (data.year == 2016) {
+                    salesTotalData2016 =
+                        salesTotalData2016 + parseInt(data["total"]);
                 }
-                if (data.year == 2017){    
-                    salesTotalData2017=salesTotalData2017+parseInt(data["total"]);
+                if (data.year == 2017) {
+                    salesTotalData2017 =
+                        salesTotalData2017 + parseInt(data["total"]);
                 }
-                if (data.year == 2018){
-                    salesTotalData2018=salesTotalData2018+parseInt(data["total"]);
+                if (data.year == 2018) {
+                    salesTotalData2018 =
+                        salesTotalData2018 + parseInt(data["total"]);
                 }
-                if (data.year == 2019){
-                    salesTotalData2019=salesTotalData2019+parseInt(data["total"]);
+                if (data.year == 2019) {
+                    salesTotalData2019 =
+                        salesTotalData2019 + parseInt(data["total"]);
                 }
 
                 //salesYear.push(data.year);
@@ -81,61 +78,47 @@
             console.log("Error");
         }
 
-var chart = c3.generate({
-    bindto: '#chart',
-    data: {
-        x : 'x',
-      columns: [
-        ['x', 2015, 2016, 2017, 2018, 2019, 2020],
-        ['Ventas totales',salesTotalData2015,salesTotalData2016,salesTotalData2017,salesTotalData2018,salesTotalData2019 ],
-        ['Desempleo', 0,0,0,0,0,unemploymentTotal],
-      ],
-      axes: {
-        Desempleo: 'y2'
-      },
-      types: {
-        Desempleo: 'bar'
-      }
-    },
-    axis: {
-      y: {
-        label: {
-          text: 'Total ventas',
-          position: 'outer-middle'
-        },
-      },
-      y2: {
-        show: true,
-        label: {
-          text: 'Total desempleo',
-          position: 'outer-middle'
-        }
-      }
-    }
-});
-
+        var chart = bb.generate({
+            data: {
+                columns: [
+                    ["data1", 30, 200, 100, 400, 150, 250],
+                    ["data2", 130, 100, 140, 200, 150, 50],
+                ],
+                type: "bar", // for ESM specify as: bar()
+            },
+            bar: {
+                width: {
+                    ratio: 0.5,
+                },
+            },
+            bindto: "#barChart",
+        });
     }
 </script>
 
 <svelte:head>
-
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.4.11/d3.js" on:load={getDataGraph}></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.11/c3.js" on:load={getDataGraph}></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.11/c3.css" rel="stylesheet" type="text/css">
+    <!-- Step 1) Load D3.js -->
+    <script src="https://d3js.org/d3.v6.min.js" on:load={getDataGraph}></script>
+    <link
+        rel="stylesheet"
+        href="https://naver.github.io/billboard.js/release/latest/dist/theme/insight.min.css"
+    />
 </svelte:head>
 
 <main>
     <div class="container">
         <Nav>
             <NavItem>
-                <a href="/#/Integrations"><Button style="margin: 0 5px 10px 50px;" color="primary">Volver</Button></a>
+                <a href="/#/Integrations"
+                    ><Button style="margin: 0 5px 10px 50px;" color="primary"
+                        >Volver</Button
+                    ></a
+                >
             </NavItem>
         </Nav>
-     </div> 
-     <div id="chart"></div>
-  
+    </div>
+    <div id="chart" />
 </main>
 
 <style>
-
 </style>
